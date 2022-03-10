08bd9230f1ae0ac5bef4b8eff954a2654e21769e
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-10 21:46:02 +0530

    added Uc-1-max-int problem

---
+++ Day-13-Usecases-Generics/MaxNumberCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Usecases_Generics
+{
+    internal class MaxNumberCheck
+    {
+        public static int MaxNumbermethod_Int(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("double numbers firstNumber,secondNumber,thirdNumber are same");
+
+        }
+
+    }
+}
---
+++ Day-13-Usecases-Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day_13_Usecases_Generics
+{
+    internal class Day_13_uc_Generics
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UseCase 1 Find Max Int  Number");
+
+            int intOutput = MaxNumberCheck.MaxNumbermethod_Int(11, 22, 33);
+            Console.Write(intOutput);
+
+        }
+    }
+}
